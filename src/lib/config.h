9e8182d7bdfbc0eab20996dac6136f67d3c440df
Marc Brevoort <kleinebre@hotmail.com>
2020-06-16 20:15:07 +0100

    Use standard integer types

--- src/lib/config.h
+++ src/lib/config.h
@@ -1,11 +1,6 @@
-#ifdef WINDOWS
-#	define __sint64 signed __int64
-#	define __uint64 unsigned __int64
-#else
-#	define __sint64 signed long long
-#	define __uint64 unsigned long long
-#endif
-
-#define __sint32 signed long
-#define __uint32 unsigned long
+#include <stdint.h>
+#define __sint32 int32_t
+#define __uint32 uint32_t
+#define __sint64 int64_t
+#define __uint64 uint64_t
 
